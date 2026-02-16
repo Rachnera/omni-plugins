f4398aeb2e225dfd9a893430cfd02e501194ce2b
Rachnera <arachne.silk@gmail.com>
2026-02-16 16:50:26 +0100

    Add (barely configured) Prettier

--- BattleSpeed.js
+++ BattleSpeed.js
@@ -2,12 +2,12 @@
 // BattleSpeed.js
 //=============================================================================
 /*:
-* @target MZ
-* @plugindesc Allow to speed up battle animations up to x4
-* @author Rachnera
-*/
+ * @target MZ
+ * @plugindesc Allow to speed up battle animations up to x4
+ * @author Rachnera
+ */
 
-(()=> {
+(() => {
   // TODO It would be nice to be able to change this variable directly from the plugin manager
   const maxBattleSpeed = 4;
   const battleSpeedMenuText = "Combat speed";
@@ -16,27 +16,27 @@
 
   /* Code to load and save battle speed alongside the other options */
 
-  const battleSpeedSymbol = 'battleSpeed';
+  const battleSpeedSymbol = "battleSpeed";
 
   Object.defineProperty(ConfigManager, battleSpeedSymbol, {
-    get: function() {
+    get: function () {
       return battleSpeed;
     },
-    set: function(value) {
+    set: function (value) {
       battleSpeed = value;
     },
-    configurable: true
+    configurable: true,
   });
 
   const alias_Rachnera_BattleSpeed_ConfigManager_makeData = ConfigManager.makeData;
-  ConfigManager.makeData = function() {
+  ConfigManager.makeData = function () {
     const config = alias_Rachnera_BattleSpeed_ConfigManager_makeData.call(this);
     config[battleSpeedSymbol] = battleSpeed;
     return config;
   };
 
   const alias_Rachnera_BattleSpeed_ConfigManager_applyData = ConfigManager.applyData;
-  ConfigManager.applyData = function(config) {
+  ConfigManager.applyData = function (config) {
     alias_Rachnera_BattleSpeed_ConfigManager_applyData.call(this, config);
     battleSpeed = 1;
     if (battleSpeedSymbol in config) {
@@ -47,13 +47,13 @@
   /* Code to have battle speed show up in the options menu (and be configurable there) */
 
   const alias_Rachnera_BattleSpeed_Window_Options_makeCommandList = Window_Options.prototype.makeCommandList;
-  Window_Options.prototype.makeCommandList = function() {
+  Window_Options.prototype.makeCommandList = function () {
     alias_Rachnera_BattleSpeed_Window_Options_makeCommandList.call(this);
     this.addCommand(battleSpeedMenuText, battleSpeedSymbol);
   };
 
   const alias_Rachnera_BattleSpeed_Window_Options_statusText = Window_Options.prototype.statusText;
-  Window_Options.prototype.statusText = function(index) {
+  Window_Options.prototype.statusText = function (index) {
     const symbol = this.commandSymbol(index);
 
     if (symbol === battleSpeedSymbol) {
@@ -61,10 +61,10 @@
     }
 
     return alias_Rachnera_BattleSpeed_Window_Options_statusText.call(this, index);
-  }
+  };
 
   const alias_Rachnera_BattleSpeed_Window_Options_processOk = Window_Options.prototype.processOk;
-  Window_Options.prototype.processOk = function() {
+  Window_Options.prototype.processOk = function () {
     const symbol = this.commandSymbol(this.index());
 
     if (symbol === battleSpeedSymbol) {
@@ -79,7 +79,7 @@
   };
 
   const alias_Rachnera_BattleSpeed_Window_Options_cursorRight = Window_Options.prototype.cursorRight;
-  Window_Options.prototype.cursorRight = function() {
+  Window_Options.prototype.cursorRight = function () {
     const symbol = this.commandSymbol(this.index());
 
     if (symbol === battleSpeedSymbol) {
@@ -94,7 +94,7 @@
   };
 
   const alias_Rachnera_BattleSpeed_Window_Options_cursorLeft = Window_Options.prototype.cursorLeft;
-  Window_Options.prototype.cursorLeft = function() {
+  Window_Options.prototype.cursorLeft = function () {
     const symbol = this.commandSymbol(this.index());
 
     if (symbol === battleSpeedSymbol) {
