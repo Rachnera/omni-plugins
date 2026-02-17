9a460f1cbe450e7cdd67f4864ef011917ce1af8e
Rachnera <arachne.silk@gmail.com>
2026-02-17 16:14:20 +0100

    Fix invisible battlers

---
+++ AssortedBugfixes.js
@@ -0,0 +1,23 @@
+//=============================================================================
+// AssortedBugfixes.js
+//=============================================================================
+/*:
+ * @target MZ
+ * @plugindesc For minor bugs that aren't worth getting their own plugins each
+ * @author Rachnera
+ * @help
+ * =====
+ * Bug #1: Battlers sometimes becoming transparent
+ * Cause: LvMZ_PartySizeControl plugin command did not always cast number-like
+ * string inputs to numbers. And in JS, 2+2=4 but 2+"2"=22.
+ * Retroactive fix: Yes
+ * =====
+ */
+(() => {
+  // Bug #1
+  // Sanitize the output rather than the input to salvage saves with a bad value
+  const alias_Game_Party_maxBattleMembers = Game_Party.prototype.maxBattleMembers;
+  Game_Party.prototype.maxBattleMembers = function () {
+    return Number(alias_Game_Party_maxBattleMembers.call(this));
+  };
+})();
