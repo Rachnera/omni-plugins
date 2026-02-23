38adb69b1d50d4526cda09197d13b33a6f721154
Rachnera <arachne.silk@gmail.com>
2026-02-23 20:48:40 +0100

    More compact battle display

---
+++ CompactSkillDescription.js
@@ -0,0 +1,54 @@
+//=============================================================================
+// CompactSkillDescription.js
+//=============================================================================
+/*:
+ * @target MZ
+ * @plugindesc Merge skill selection and skill description
+ * @author Rachnera
+ *
+ * @help
+ * The main idea is to get rid of that horrible help box at the top that takes
+ * so much space and push around other elements each time it shows up.
+ * This plugin is not really subtle in its behavior and will override several
+ * of VisuStella Battle Core parameters without a second care.
+ */
+
+(() => {
+  Window_BattleSkill.prototype.maxCols = function () {
+    return 1;
+  };
+
+  Scene_Battle.prototype.skillWindowRect = function () {
+    const ww = Graphics.boxWidth * 0.45;
+    const wh = this.windowAreaHeight();
+
+    const wx = 0;
+    const wy = Graphics.boxHeight - wh;
+
+    return new Rectangle(wx, wy, ww, wh);
+  };
+
+  Scene_Battle.prototype.helpWindowRect = function () {
+    const ww = Graphics.boxWidth * 0.55;
+    const wh = this.windowAreaHeight();
+
+    const wx = Graphics.boxWidth * 0.45;
+    const wy = Graphics.boxHeight - wh;
+
+    return new Rectangle(wx, wy, ww, wh);
+  };
+
+  // For some reason escaping me, VisuStella Battle Core disables RMMZ
+  // standard behavior of hiding unused windows in battle, meaning they show up
+  // in the background of every transparent menu. This restores the default.
+  Window_ActorCommand.prototype.hide = function () {
+    Window_StatusBase.prototype.hide.call(this);
+  };
+  Window_BattleStatus.prototype.hide = function () {
+    Window_StatusBase.prototype.hide.call(this);
+  };
+
+  Window_CTB_TurnOrder.prototype.updatePosition = function () {
+    return;
+  };
+})();
