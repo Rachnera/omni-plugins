b7162a7c750740717e68af5a595024061058d9e2
Rachnera <arachne.silk@gmail.com>
2026-02-28 18:30:31 +0100

    Attempt at instaWin utility function

---
+++ UtilityFunctions.js
@@ -0,0 +1,22 @@
+//=============================================================================
+// AssortedBugfixes.js
+//=============================================================================
+/*:
+ * @target MZ
+ * @plugindesc Various pieces of code to be called through the Script command
+ * @author Rachnera
+ * @help
+ * =====
+ * instaWinBattle(troopId): Call the code to trigger a battle against the troop
+ * with the given id, immediately kill all enemies, and award rewards but
+ * without ever showing any battle-related screen.
+ * =====
+ */
+
+const instaWinBattle = (troopId) => {
+  BattleManager.setup(troopId);
+  $gameTroop.members().forEach((enemy) => enemy.addNewState(1));
+  BattleManager.makeRewards();
+  BattleManager.gainRewards();
+  BattleManager.endBattle(0);
+};
