9afb283892ca2d36b45873f19f39e8befdfc0f4f
Rachnera <arachne.silk@gmail.com>
2026-03-01 14:46:48 +0100

    Print insta combat result into Gab Window

--- UtilityFunctions.js
+++ UtilityFunctions.js
@@ -10,14 +10,65 @@
  * instaWinBattle(troopId): Call the code to trigger a battle against the troop
  * with the given id, immediately kill all enemies, and award rewards but
  * without ever showing any battle-related screen.
+ * If VisuStella MZ - Gab Window is enabled, messages will be print in the Gab
+ * Window instead of the standard one.
  * =====
  */
 
 const instaWinBattle = (troopId) => {
+  GameMessageHacks.pipeIntoGab();
+
   BattleManager.setup(troopId);
   $gameTroop.members().forEach((enemy) => enemy.addNewState(1));
   BattleManager.makeRewards();
   BattleManager.displayRewards();
   BattleManager.gainRewards();
   BattleManager.endBattle(0);
+
+  GameMessageHacks.restoreDefaults();
 };
+
+const GameMessageHacks = {};
+
+(() => {
+  let pipeGameMessageIntoGap = false;
+  let gabBuffer = [];
+
+  const alias_Game_Message_add = Game_Message.prototype.add;
+  Game_Message.prototype.add = function (text) {
+    if (!pipeGameMessageIntoGap) {
+      return alias_Game_Message_add.call(this, text);
+    }
+
+    gabBuffer.push(text);
+  };
+
+  const alias_Game_Message_newPage = Game_Message.prototype.newPage;
+  Game_Message.prototype.newPage = function () {
+    if (!pipeGameMessageIntoGap) {
+      return alias_Game_Message_newPage.call(this);
+    }
+
+    GameMessageHacks.printGab();
+  };
+
+  GameMessageHacks.pipeIntoGab = () => {
+    pipeGameMessageIntoGap = true;
+  };
+
+  GameMessageHacks.printGab = () => {
+    if (gabBuffer.length === 0) {
+      return;
+    }
+
+    SceneManager._scene.startGabWindow({ Text: "<WordWrap>" + gabBuffer.join("\n") + "</WordWrap>" });
+
+    gabBuffer = [];
+  };
+
+  GameMessageHacks.restoreDefaults = () => {
+    GameMessageHacks.printGab();
+
+    pipeGameMessageIntoGap = false;
+  };
+})();
