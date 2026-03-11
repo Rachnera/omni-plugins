2aa3dd1e1893561f71bc7a01f8c87615a94e5dea
Rachnera <arachne.silk@gmail.com>
2026-03-11 16:03:28 +0100

    Complete the merge of description and selection

--- CompactSkillDescription.js
+++ CompactSkillDescription.js
@@ -18,20 +18,32 @@
     return 1;
   };
 
-  const commandWindowWidth = 420;
-
-  Scene_Battle.prototype.skillWindowRect = function () {
-    const ww = commandWindowWidth;
-    const wh = this.windowAreaHeight();
-
-    const wx = Graphics.boxWidth - commandWindowWidth;
-    const wy = Graphics.boxHeight - wh;
-
-    return new Rectangle(wx, wy, ww, wh);
+  Window_BattleSkill.prototype.itemHeight = function () {
+    return (this.lineHeight() + 8) * 3;
   };
 
-  Scene_Battle.prototype.helpWindowRect = function () {
-    const ww = Graphics.boxWidth - commandWindowWidth;
+  Window_BattleSkill.prototype.drawItem = function (index) {
+    const skill = this.itemAt(index);
+    if (!skill) {
+      return;
+    }
+
+    const costWidth = this.costWidth();
+    const rect = this.itemLineRect(index);
+    const yOffset = this.itemHeight() / 3 - this.itemPadding();
+
+    this.changePaintOpacity(this.isEnabled(skill));
+
+    this.drawItemName(skill, rect.x, rect.y - yOffset, rect.width - costWidth);
+    this.drawSkillCost(skill, rect.x, rect.y - yOffset, rect.width);
+
+    this.drawTextEx(skill.description, rect.x, rect.y - yOffset + this.lineHeight(), this.width);
+
+    this.changePaintOpacity(1);
+  };
+
+  Scene_Battle.prototype.skillWindowRect = function () {
+    const ww = Graphics.boxWidth - this._actorCommandWindow.width;
     const wh = this.windowAreaHeight();
 
     const wx = 0;
@@ -40,12 +52,16 @@
     return new Rectangle(wx, wy, ww, wh);
   };
 
+  const alias_Window_BattleSkill_show = Window_BattleSkill.prototype.show;
+  Window_BattleSkill.prototype.show = function () {
+    alias_Window_BattleSkill_show.call(this);
+
+    this.hideHelpWindow();
+  };
+
   // For some reason escaping me, VisuStella Battle Core disables RMMZ
   // standard behavior of hiding unused windows in battle, meaning they show up
   // in the background of every transparent menu. This restores the default.
-  Window_ActorCommand.prototype.hide = function () {
-    Window_StatusBase.prototype.hide.call(this);
-  };
   Window_BattleStatus.prototype.hide = function () {
     Window_StatusBase.prototype.hide.call(this);
   };
