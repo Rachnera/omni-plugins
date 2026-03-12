6d4c20d760b9c8ef74ca94376cb87c0547f8e0e4
Rachnera <arachne.silk@gmail.com>
2026-03-12 07:54:59 +0100

    Back to old version

--- CompactSkillDescription.js
+++ CompactSkillDescription.js
@@ -18,32 +18,8 @@
     return 1;
   };
 
-  Window_BattleSkill.prototype.itemHeight = function () {
-    return (this.lineHeight() + 8) * 3;
-  };
-
-  Window_BattleSkill.prototype.drawItem = function (index) {
-    const skill = this.itemAt(index);
-    if (!skill) {
-      return;
-    }
-
-    const costWidth = this.costWidth();
-    const rect = this.itemLineRect(index);
-    const yOffset = this.itemHeight() / 3 - this.itemPadding();
-
-    this.changePaintOpacity(this.isEnabled(skill));
-
-    this.drawItemName(skill, rect.x, rect.y - yOffset, rect.width - costWidth);
-    this.drawSkillCost(skill, rect.x, rect.y - yOffset, rect.width);
-
-    this.drawTextEx(skill.description, rect.x, rect.y - yOffset + this.lineHeight(), this.width);
-
-    this.changePaintOpacity(1);
-  };
-
   Scene_Battle.prototype.skillWindowRect = function () {
-    const ww = Graphics.boxWidth - this._actorCommandWindow.width;
+    const ww = Graphics.boxWidth * 0.45;
     const wh = this.windowAreaHeight();
 
     const wx = 0;
@@ -52,16 +28,22 @@
     return new Rectangle(wx, wy, ww, wh);
   };
 
-  const alias_Window_BattleSkill_show = Window_BattleSkill.prototype.show;
-  Window_BattleSkill.prototype.show = function () {
-    alias_Window_BattleSkill_show.call(this);
+  Scene_Battle.prototype.helpWindowRect = function () {
+    const ww = Graphics.boxWidth * 0.55;
+    const wh = this.windowAreaHeight();
 
-    this.hideHelpWindow();
+    const wx = Graphics.boxWidth * 0.45;
+    const wy = Graphics.boxHeight - wh;
+
+    return new Rectangle(wx, wy, ww, wh);
   };
 
   // For some reason escaping me, VisuStella Battle Core disables RMMZ
   // standard behavior of hiding unused windows in battle, meaning they show up
   // in the background of every transparent menu. This restores the default.
+  Window_ActorCommand.prototype.hide = function () {
+    Window_StatusBase.prototype.hide.call(this);
+  };
   Window_BattleStatus.prototype.hide = function () {
     Window_StatusBase.prototype.hide.call(this);
   };
