b69dd1e9a9130d0fa775fc7772075049c339b082
Rachnera <arachne.silk@gmail.com>
2026-03-12 22:27:27 +0100

    Very basic waiting skill

---
+++ WaitSkills.js
@@ -0,0 +1,55 @@
+//=============================================================================
+// WaitSkills.js
+//=============================================================================
+/*:
+ * @target MZ
+ * @plugindesc Notetags to make "wait" skills
+ * @author Rachnera
+ * @help
+ * All notetags are to be added to the relevant skills
+ *
+ * To wait until the end of the next action:
+ * <JS Pre-End Action>
+ *   WS.waitOneAction(user)
+ * </JS Pre-End Action>
+ */
+
+const WS = {};
+
+(() => {
+  let actorToMove = null;
+
+  WS.waitOneAction = (actor) => {
+    actorToMove = actor;
+  };
+
+  const moveIfNeeded = () => {
+    if (!actorToMove) {
+      return;
+    }
+
+    actorToMove.setTurnOrderCTB(1);
+    actorToMove = null;
+  };
+
+  const alias_BattleManager_selectNextActor = BattleManager.selectNextActor;
+  BattleManager.selectNextActor = function () {
+    moveIfNeeded();
+
+    alias_BattleManager_selectNextActor.call(this);
+  };
+
+  const alias_BattleManager_processTurn = BattleManager.processTurn;
+  BattleManager.processTurn = function () {
+    moveIfNeeded();
+
+    alias_BattleManager_processTurn.call(this);
+  };
+
+  const alias_BattleManager_endBattle = BattleManager.endBattle;
+  BattleManager.endBattle = function (result) {
+    actorToMove = null; // Security cleanup
+
+    alias_BattleManager_endBattle.call(this, result);
+  };
+})();
