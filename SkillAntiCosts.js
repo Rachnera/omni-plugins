4823b1ffb5930d2681c71a84f4cb7b510e4b2735
Rachnera <arachne.silk@gmail.com>
2026-03-15 11:09:06 +0100

    Make text configurable

--- SkillAntiCosts.js
+++ SkillAntiCosts.js
@@ -6,12 +6,20 @@
  * @plugindesc Show TP regain of skills similar to TP costs
  * @author Rachnera
  *
+ * @param Text format
+ * @desc %1 is automatically replaced with the appropriate value.
+ * @default \FS[22] \C[14] +%1 Energy
+ *
  * @help
+ * All standard text codes should be supported within the text format option:
+ * https://www.yanfly.moe/wiki/Category:Text_Codes_(MZ)
+ *
  * The case where a skill both costs and restores TP to its user isn't covered.
  */
 
 (() => {
-  // TODO
+  const params = PluginManager.parameters("SkillAntiCosts");
+  const textFormat = params["Text format"] || "\\FS[22] \\C[14] +%1 Energy";
 
   const alias_Window_Base_drawSkillCost = Window_Base.prototype.drawSkillCost;
   Window_Base.prototype.drawSkillCost = function (actor, skill, x, y, width) {
@@ -24,7 +32,7 @@
 
     const tpGain = Number(result[1]);
 
-    const text = "\\FS[22] \\C[14] +%1 Energy".format(tpGain);
+    const text = textFormat.format(tpGain);
 
     this.drawTextEx(text, x + width - this.textSizeEx(text).width, y, width);
     this.resetFontSettings();
