4c58d3c326afaa5f23dd1922ba31a0eac7382d12
Rachnera <arachne.silk@gmail.com>
2026-03-23 18:58:57 +0100

    Add icon scale option

--- BattleUIAdjusters.js
+++ BattleUIAdjusters.js
@@ -3,7 +3,7 @@
 //=============================================================================
 /*:
  * @target MZ
- * @plugindesc Various on/off options for trying to make battles more readable
+ * @plugindesc Various options for trying to make battles more readable
  * @author Rachnera
  *
  * @param Selected enemy in the foreground
@@ -11,11 +11,17 @@
  * @type boolean
  * @default false
  *
+ * @param Scale of state icons
+ * @default 1.0
+ *
  * @help
  * All options are off per default.
  *
  * Enemies' health bars and names are on a different layer from the other
  * enemy-related sprites, which may require further consideration.
+ *
+ * This plugin should probably be eventually merged, partially or totally,
+ * with relevant features for easier maintainability.
  *
  * To only display enemy's names on select:
  * VisuMZ_1_BattleCore -> Enemy Battler Settings -> Name > Name Visibility
@@ -27,41 +33,77 @@
 (() => {
   const params = PluginManager.parameters("BattleUIAdjusters");
   const selectedEnemyInFront = params["Selected enemy in the foreground"];
+  const stateIconScale = Number(params["Scale of state icons"] || 1.0);
 
   const alias_Sprite_Enemy_update = Sprite_Enemy.prototype.update;
   Sprite_Enemy.prototype.update = function () {
     alias_Sprite_Enemy_update.call(this);
 
-    if (!selectedEnemyInFront) {
-      return;
-    }
-
     if (!this._battler) {
       return;
     }
 
-    if (this._battler.isSelected() && !this.pushedToForeground) {
-      // MZ does not use z-index. Instead, it's the order in which sprites
-      // are added to the canvas who's on top.
+    this.pushForwardIfSelected();
+    this.scaleDownStateIcons();
+  };
 
-      const spriteset = SceneManager._scene._spriteset;
-
-      spriteset._enemySprites.forEach((sprite) => spriteset._battleField.removeChild(sprite));
-
-      spriteset._enemySprites.sort((a, b) => {
-        if (a === this) {
-          return 1;
-        }
-        if (b === this) {
-          return -1;
-        }
-        return 0;
-      });
-
-      spriteset._enemySprites.forEach((sprite) => {
-        spriteset._battleField.addChild(sprite);
-        sprite.pushedToForeground = sprite === this;
-      });
+  Sprite_Enemy.prototype.pushForwardIfSelected = function () {
+    if (!selectedEnemyInFront) {
+      return;
     }
+
+    if (!this._battler.isSelected()) {
+      return;
+    }
+
+    if (this.pushedToForeground) {
+      return;
+    }
+
+    // MZ does not use z-index. Instead, it's the order in which sprites
+    // are added to the canvas who's on top.
+
+    const spriteset = SceneManager._scene._spriteset;
+
+    spriteset._enemySprites.forEach((sprite) => spriteset._battleField.removeChild(sprite));
+
+    spriteset._enemySprites.sort((a, b) => {
+      if (a === this) {
+        return 1;
+      }
+      if (b === this) {
+        return -1;
+      }
+      return 0;
+    });
+
+    spriteset._enemySprites.forEach((sprite) => {
+      spriteset._battleField.addChild(sprite);
+      sprite.pushedToForeground = sprite === this;
+    });
+  };
+
+  Sprite_Enemy.prototype.scaleDownStateIcons = function () {
+    const scale = stateIconScale;
+
+    if (scale === 1 || scale === 1.0) {
+      return;
+    }
+
+    if (!this._staticStateIconSprites) {
+      return;
+    }
+
+    // FIXME Kind of weird and fragile to have this code there
+    // Should be an option of StateIconRework instead
+
+    const visibleIcons = this._staticStateIconSprites.filter((sprite) => sprite.visible);
+    const padding = Math.floor(4 * scale) || 1;
+    const widthWithPadding = ImageManager.iconWidth * scale + padding * 2;
+
+    visibleIcons.forEach((sprite, i) => {
+      sprite.scale.x = sprite.scale.y = scale;
+      sprite.x = -((visibleIcons.length - 1) * widthWithPadding) / 2 + i * widthWithPadding;
+    });
   };
 })();
