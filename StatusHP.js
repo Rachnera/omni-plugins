72e2ad0ba62cfc7c41434e23a52969d7ceba4fc0
Rachnera <arachne.silk@gmail.com>
2026-07-21 13:12:12 +0200

    Kinda show the right icon (only work for two hits)

--- StatusHP.js
+++ StatusHP.js
@@ -16,6 +16,7 @@
 (() => {
   const specialStateId = 13;
   const specialEnemyId = 93;
+  const initialStateHP = 200;
 
   const alias_Game_Action_itemEffectAddState = Game_Action.prototype.itemEffectAddState;
   Game_Action.prototype.itemEffectAddState = function (target, effect) {
@@ -31,7 +32,7 @@
   Game_Battler.prototype.addNewState = function (stateId) {
     if (stateId === specialStateId && this instanceof Game_Enemy && this._enemyId === specialEnemyId) {
       if (!this._specialStateHP || this._specialStateHP < 0) {
-        this._specialStateHP = 200;
+        this._specialStateHP = initialStateHP;
       }
 
       this._specialStateHP -= 100;
@@ -41,5 +42,49 @@
     }
 
     alias_Game_Battler_addNewState.call(this, stateId);
+  };
+
+  const alias_Sprite_Enemy_update = Sprite_Enemy.prototype.update;
+  Sprite_Enemy.prototype.update = function () {
+    alias_Sprite_Enemy_update.call(this);
+
+    if (this._battler?._enemyId !== specialEnemyId) {
+      return;
+    }
+
+    if (!this._staticStateIconSprites || !this._staticStateIconSprites.length) {
+      return;
+    }
+
+    const doNotDisplay =
+      !this._battler._specialStateHP ||
+      this._battler._specialStateHP <= 0 ||
+      this._battler._specialStateHP >= initialStateHP ||
+      !this._battler.isAlive();
+    const iconToShow = 80;
+    const currentlyDisplayed = this._staticStateIconSprites.some((sprite) => sprite._iconIndex === iconToShow);
+
+    // The way the code currently works, there's an automatic clean up when the state is actually applied
+    if (doNotDisplay || currentlyDisplayed) {
+      return;
+    }
+
+    const firstAvailableSpriteIndex = this._staticStateIconSprites.findIndex((sprite) => !sprite._iconIndex);
+
+    if (firstAvailableSpriteIndex === -1) {
+      return;
+    }
+
+    const firstAvailableSprite = this._staticStateIconSprites[firstAvailableSpriteIndex];
+
+    firstAvailableSprite._iconIndex = iconToShow;
+    firstAvailableSprite.updateFrame();
+    firstAvailableSprite.show();
+
+    for (let i = 0; i < firstAvailableSpriteIndex + 1; i++) {
+      const sprite = this._staticStateIconSprites[i];
+
+      sprite.readjustPosition(i, firstAvailableSpriteIndex + 1, this);
+    }
   };
 })();
