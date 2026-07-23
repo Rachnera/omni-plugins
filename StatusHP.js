e095ca35272808cfe2ba71c8fcf019be2f413854
Rachnera <arachne.silk@gmail.com>
2026-07-23 13:02:16 +0200

    Extend to any number of states/enemies

--- StatusHP.js
+++ StatusHP.js
@@ -6,20 +6,16 @@
  * @plugindesc Require a certain number of hits for a state to stick
  * @author Rachnera
  *
- * @param Enemy id
- * @desc The one enemy in the game that will be affected by that mechanic
- *
- * @param State id
- * @desc The one state in the game that will be affected by that mechanic
- *
- * @param State HP
- * @desc How many "HP" the state has. Each hit removes 100 of them. Applied at 0.
- * @default 200
- *
  * @help
- * Prototype:
- * - Only work for one specific status effect against one specific enemy
- * - Enemy must be vulnerable to the state
+ * Relevant enemies must be configured with the following notetag:
+ * <Partial State Resist: x, y>
+ * Where x is the id of the state and y the base HP of it. Each attempt at
+ * applying it removes 100¹ "state HP", so a y of 200 means two hits are needed
+ * for it to stick, 300 tranlastes into three hits etc.
+ * Tag can be put multiple times with multiple states for the same enemy.
+ *
+ * Note: The enemy must be vulnerable to the state for this to work. Otherwise,
+ * its immunity takes priority.
  *
  * For each state that can have HP, a gradient displaying its HP bar in sixteen
  * variants of fullness (from 0 to 15/16) must be added as a newline in
@@ -28,14 +24,30 @@
  * <Partial Success Zero Icon: x>
  * Where x is the id of the first icon of the relevant line (thus should always
  * be a multiple of 16)
+ *
+ * ¹Value not configurable at the moment.
  */
 
 (() => {
   const params = PluginManager.parameters("StatusHP");
 
-  const specialStateId = parseInt(params["State id"]);
-  const specialEnemyId = parseInt(params["Enemy id"]);
-  const initialStateHP = parseInt(params["State HP"]) || 200;
+  const getStatesHp = (enemyId) => {
+    const enemy = $dataEnemies[enemyId];
+
+    if (!enemy) {
+      return {};
+    }
+
+    const regexp = /<Partial State Resist:\s*([0-9]+)\s*,\s*([0-9]+)\s*>/gms;
+
+    const result = enemy.note.matchAll(regexp);
+
+    const groupedByStateId = {};
+    for (let match of result) {
+      groupedByStateId[parseInt(match[1])] = parseInt(match[2]);
+    }
+    return groupedByStateId;
+  };
 
   const getZeroIconIndex = (stateId) => {
     const state = $dataStates[stateId];
@@ -65,41 +77,24 @@
     return Array.from(Array(16).keys()).map((i) => i + zeroIndex);
   };
 
-  const shouldAbort = !specialStateId || !specialEnemyId || !initialStateHP;
-
-  const alias_Game_Action_itemEffectAddState = Game_Action.prototype.itemEffectAddState;
-  Game_Action.prototype.itemEffectAddState = function (target, effect) {
-    alias_Game_Action_itemEffectAddState.call(this, target, effect);
-
-    if (shouldAbort) {
-      return;
-    }
-
-    const isEnemy = target instanceof Game_Enemy;
-    if (!isEnemy || target._enemyId !== specialEnemyId) {
-      return;
-    }
-  };
-
   const alias_Game_Battler_addNewState = Game_Battler.prototype.addNewState;
   Game_Battler.prototype.addNewState = function (stateId) {
-    if (shouldAbort) {
-      return alias_Game_Battler_addNewState.call(this, stateId);
-    }
+    if (this instanceof Game_Enemy) {
+      const statesWithHp = getStatesHp(this._enemyId);
+      if (stateId in statesWithHp) {
+        if (!this._specialStateHP) {
+          this._specialStateHP = {};
+        }
 
-    if (stateId === specialStateId && this instanceof Game_Enemy && this._enemyId === specialEnemyId) {
-      if (!this._specialStateHP) {
-        this._specialStateHP = {};
-      }
+        // Same behavior if not set or zero
+        if (!this._specialStateHP[stateId] || this._specialStateHP[stateId] < 0) {
+          this._specialStateHP[stateId] = statesWithHp[stateId];
+        }
 
-      // Same behavior if not set or zero
-      if (!this._specialStateHP[stateId] || this._specialStateHP[stateId] < 0) {
-        this._specialStateHP[stateId] = initialStateHP;
-      }
-
-      this._specialStateHP[stateId] -= 100;
-      if (this._specialStateHP[stateId] > 0) {
-        return;
+        this._specialStateHP[stateId] -= 100;
+        if (this._specialStateHP[stateId] > 0) {
+          return;
+        }
       }
     }
 
@@ -110,62 +105,64 @@
   Sprite_Enemy.prototype.update = function () {
     alias_Sprite_Enemy_update.call(this);
 
-    if (shouldAbort) {
+    const isEnemy = this._battler instanceof Game_Enemy;
+    if (!isEnemy) {
       return;
     }
 
-    if (this._battler?._enemyId !== specialEnemyId) {
+    if (!this._staticStateIconSprites?.length) {
       return;
     }
 
-    if (!this._staticStateIconSprites || !this._staticStateIconSprites.length) {
+    if (!this._battler._specialStateHP) {
       return;
     }
 
-    if (!this._battler._specialStateHP || !(specialStateId in this._battler._specialStateHP)) {
-      return;
-    }
+    const statesWithHp = getStatesHp(this._battler._enemyId);
 
-    const doNotDisplay =
-      !this._battler._specialStateHP[specialStateId] ||
-      this._battler._specialStateHP[specialStateId] <= 0 ||
-      this._battler._specialStateHP[specialStateId] >= initialStateHP ||
-      !this._battler.isAlive();
+    Object.keys(this._battler._specialStateHP)
+      .map((n) => parseInt(n))
+      .forEach((stateId) => {
+        const currentHp = this._battler._specialStateHP[stateId];
+        const maxHp = statesWithHp[stateId];
 
-    const relevantIcons = getGradient(specialStateId);
+        const doNotDisplay = !currentHp || currentHp < 0 || currentHp >= maxHp || !this._battler.isAlive();
 
-    const iconToShow = relevantIcons[Math.floor((16 * this._battler._specialStateHP[specialStateId]) / initialStateHP)];
-    const currentlyDisplayed = this._staticStateIconSprites.some((sprite) => sprite._iconIndex === iconToShow);
+        const relevantIcons = getGradient(stateId);
 
-    // The way the code currently works, there's an automatic clean up when the state is actually applied
-    if (doNotDisplay || currentlyDisplayed) {
-      return;
-    }
+        const iconToShow = relevantIcons[Math.floor((16 * currentHp) / maxHp)];
+        const currentlyDisplayed = this._staticStateIconSprites.some((sprite) => sprite._iconIndex === iconToShow);
 
-    // First searching if there's a partial state icon already
-    let firstAvailableSpriteIndex = this._staticStateIconSprites.findIndex((sprite) => {
-      return relevantIcons.includes(sprite._iconIndex);
-    });
+        // The way the code currently works, there's an automatic clean up when the state is actually applied
+        if (doNotDisplay || currentlyDisplayed) {
+          return;
+        }
 
-    // If not, look for first avaiable sprite
-    if (firstAvailableSpriteIndex === -1) {
-      firstAvailableSpriteIndex = this._staticStateIconSprites.findIndex((sprite) => !sprite._iconIndex);
-    }
+        // First searching if there's a partial state icon already
+        let firstAvailableSpriteIndex = this._staticStateIconSprites.findIndex((sprite) => {
+          return relevantIcons.includes(sprite._iconIndex);
+        });
 
-    if (firstAvailableSpriteIndex === -1) {
-      return;
-    }
+        // If not, look for first avaiable sprite
+        if (firstAvailableSpriteIndex === -1) {
+          firstAvailableSpriteIndex = this._staticStateIconSprites.findIndex((sprite) => !sprite._iconIndex);
+        }
 
-    const firstAvailableSprite = this._staticStateIconSprites[firstAvailableSpriteIndex];
+        if (firstAvailableSpriteIndex === -1) {
+          return;
+        }
 
-    firstAvailableSprite._iconIndex = iconToShow;
-    firstAvailableSprite.updateFrame();
-    firstAvailableSprite.show();
+        const firstAvailableSprite = this._staticStateIconSprites[firstAvailableSpriteIndex];
 
-    for (let i = 0; i < firstAvailableSpriteIndex + 1; i++) {
-      const sprite = this._staticStateIconSprites[i];
+        firstAvailableSprite._iconIndex = iconToShow;
+        firstAvailableSprite.updateFrame();
+        firstAvailableSprite.show();
 
-      sprite.readjustPosition(i, firstAvailableSpriteIndex + 1, this);
-    }
+        const totalNumberOfIcons = this._staticStateIconSprites.findIndex((sprite) => !sprite._iconIndex) + 1;
+        for (let i = 0; i < totalNumberOfIcons; i++) {
+          const sprite = this._staticStateIconSprites[i];
+          sprite.readjustPosition(i, totalNumberOfIcons, this);
+        }
+      });
   };
 })();
